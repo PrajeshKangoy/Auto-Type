4c34255353f62348141cbeaa99da660252133fa1
PrajeshKangoy <theprajeshkangoy@gmail.com>
2025-04-13 13:51:50 +0545

    final code

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+
+import pyautogui
 import time
-import sys
 
-def typewriter(text):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        # time.sleep(speed)
-    print()  # Move to next line
+def type_like_human(text, delay=0.2):
+    words = text.split()
+    for word in words:
+        pyautogui.typewrite(word)
+        pyautogui.press('space')
+        time.sleep(delay)
 
-# Ask user for input
-user_input = input("Enter some text: ")
+# Wait a few seconds so you can switch to your target window
+print("You have 5 seconds to click where you want it to type...")
+time.sleep(5)
 
-# Typewrite the input
-typewriter(user_input)
+# Paste your copied text here
+text_to_type = """
+My name is ritesh, and i am the don of nepal
+"""
+
+type_like_human(text_to_type, delay=0.3)  # You can tweak delay speed
